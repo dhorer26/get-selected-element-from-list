3bfdabade884c4b934e0b6f2824a4fe2a4619183
Ravindra Dhore <ravindrad@ansh.com>
2019-11-25 12:22:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'routingdemo';
+  // tslint:disable-next-line: no-use-before-declare
+  selectedPerson: any = nameList;
+
+  // data
+  nameList = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' }
+  ];
+
+  getSelectedElement(names: nameList) {
+    this.selectedPerson = names;
+  }
+}
+
+// tslint:disable-next-line: class-name
+export class nameList {
+  id: number;
+  name: string;
 }
